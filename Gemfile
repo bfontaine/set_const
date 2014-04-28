af6fb44c394ccec13e6c6bf0c6cae10efa30db5b
Baptiste Fontaine <batifon@yahoo.fr>
2014-04-28 11:39:57 +0200

    full docs

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'silent', '~> 0.1'
 
 group :test do
   gem 'simplecov', :require => false
+  gem 'inch', '~> 0.4.2', require: false
   gem 'rake'
   gem 'test-unit'
 end
--- lib/set_const.rb
+++ lib/set_const.rb
@@ -2,6 +2,18 @@
 
 require 'silent'
 
+# Silently change a constant's value
+# @param name [String] the constant's name
+# @param value the contant's new value
+# @param opts [Hash] options. Only one key is supported for now, +:parent+. It
+#                    must be used when the constant is not global but under a
+#                    class or module.
+# @return the given value
+#
+# @example change +ARGV+’s value
+#   set_const('ARGV', ['foo', '--bar', '42'])
+# @example modify +MyModule.FOO+
+#   set_const('FOO', 'something', parent: MyModule)
 def set_const(name, value, opts={})
   parent = opts[:parent] || Object
 
